32ad0fb2ac9668cd8648f8a97defc305da5c7c6a
Alessio Marchese <alessio.marchese148@gmail.com>
2025-05-13 17:51:13 +0200

    Add project files.

---
+++ Core/Result.cs
@@ -0,0 +1,52 @@
+﻿namespace AM.Utils.Core;
+
+using System.Diagnostics.CodeAnalysis;
+public class Result<T>
+{
+    [MemberNotNullWhen(false, nameof(ErrorMessage))]
+    [MemberNotNullWhen(true, nameof(Data))]
+    public bool IsSuccessful { get; }
+    public string? ErrorMessage { get; }
+    public T? Data { get; }
+
+    private Result(bool isSuccessful, T? value, string? errorMessage)
+    {
+        IsSuccessful = isSuccessful;
+        ErrorMessage = errorMessage;
+        Data = value;
+    }
+
+    public static Result<T> Success(T value)
+        => new(true, value, null);
+
+    public static Result<T> Failure(string errorMessage)
+        => new(false, default, errorMessage);
+
+    public Result<U> ToResult<U>()
+    {
+        if (this.IsSuccessful)
+            throw new InvalidOperationException("ToResult<U> Can only be used on a failed Result.");
+
+        return Result<U>.Failure(this.ErrorMessage);
+    }
+}
+
+public class Result
+{
+    [MemberNotNullWhen(false, nameof(ErrorMessage))]
+    public bool IsSuccessful { get; }
+    public string? ErrorMessage { get; }
+
+    public Result(bool isSuccessful, string? errorMessage)
+    {
+        IsSuccessful = isSuccessful;
+        ErrorMessage = errorMessage;
+    }
+
+    public static Result Success() =>
+        new(true, null);
+
+    public static Result Failure(string errorMessage) =>
+        new(false, errorMessage);
+}
+
